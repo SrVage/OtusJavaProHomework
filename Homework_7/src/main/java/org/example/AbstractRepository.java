8ea94661e100a365689d49ec352bdd6199e6ed09
MKrampit <MKrampit@bitbucket.org>
2024-05-19 18:27:32 +0700

    Homework complete

---
+++ Homework_7/src/main/java/org/example/AbstractRepository.java
@@ -0,0 +1,151 @@
+package org.example;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+public class AbstractRepository<T> {
+    private final PreparedStatement psCreate;
+    private final PreparedStatement psSelect;
+    private final PreparedStatement psSelectById;
+    private final PreparedStatement psUpdate;
+    private final PreparedStatement psDeleteById;
+    private final PreparedStatement psDeleteAll;
+    private final List<Field> cachedGetFields;
+    private final List<Field> cachedSetFields;
+    private final Method getId;
+    private final Class<T> cls;
+
+    public AbstractRepository(
+            Class<T> cls,
+            PreparedStatement psCreate,
+            PreparedStatement psSelect,
+            PreparedStatement psSelectById,
+            PreparedStatement psUpdate,
+            PreparedStatement psDeleteById,
+            PreparedStatement psDeleteAll,
+            List<Field> cachedGetFields,
+            List<Field> cachedSetFields
+    ) throws NoSuchMethodException {
+        this.cls = cls;
+        this.psCreate = psCreate;
+        this.psSelect = psSelect;
+        this.psSelectById = psSelectById;
+        this.psUpdate = psUpdate;
+        this.psDeleteById = psDeleteById;
+        this.psDeleteAll = psDeleteAll;
+        this.cachedGetFields = cachedGetFields;
+        this.cachedSetFields = cachedSetFields;
+        getId = prepareIdField();
+    }
+
+    public void create(T entity) {
+        try {
+            for (int i = 0; i < cachedGetFields.size(); i++) {
+                var method = cls.getMethod("get"+ cachedGetFields.get(i).getName().substring(0,1).toUpperCase()+ cachedGetFields.get(i).getName().trim().substring(1));
+                psCreate.setObject(i + 1, method.invoke(entity));
+            }
+            psCreate.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<T> findAll() throws SQLException {
+        List<T> out = new ArrayList<>();
+        try (ResultSet rs = psSelect.executeQuery()) {
+            while (rs.next()) {
+                Constructor<T> constructor = cls.getDeclaredConstructor();
+                var obj = constructor.newInstance();
+                for (var field : cachedSetFields){
+                    var method = cls.getMethod("set"+field.getName().substring(0,1).toUpperCase()+field.getName().trim().substring(1), field.getType());
+                    var columnName = field.getAnnotation(RepositoryField.class).column();
+                    if (field.getType() == Long.class || field.getType() == long.class){
+                        method.invoke(obj, rs.getLong(columnName.isEmpty() ? field.getName() : columnName));
+                    }
+                    else{
+                        method.invoke(obj, rs.getString(columnName.isEmpty() ? field.getName() : columnName));
+                    }
+                }
+                out.add(obj);
+            }
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return Collections.unmodifiableList(out);
+    }
+
+    public Optional<T> findById(long id) throws SQLException {
+        psSelectById.setObject(1, id);
+        try (
+                ResultSet rs =  psSelectById.executeQuery()) {
+            while (rs.next()) {
+                Constructor<T> constructor = cls.getDeclaredConstructor();
+                var obj = constructor.newInstance();
+                for (var field : cachedSetFields){
+                    var method = cls.getMethod("set"+field.getName().substring(0,1).toUpperCase()+field.getName().trim().substring(1), field.getType());
+                    var columnName = field.getAnnotation(RepositoryField.class).column();
+                    if (field.getType() == Long.class || field.getType() == long.class){
+                        method.invoke(obj, rs.getLong(columnName.isEmpty() ? field.getName() : columnName));
+                    }
+                    else{
+                        method.invoke(obj, rs.getString(columnName.isEmpty() ? field.getName() : columnName));
+                    }
+                }
+                return Optional.of(obj);
+            }
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public void update(T entity){
+        try {
+            for (int i = 0; i < cachedGetFields.size(); i++) {
+                var method = cls.getMethod("get" +
+                        cachedGetFields.get(i).getName().substring(0, 1).toUpperCase() +
+                        cachedGetFields.get(i).getName().trim().substring(1));
+                psUpdate.setObject(i + 1, method.invoke(entity));
+            }
+            psUpdate.setObject(cachedGetFields.size() + 1, getId.invoke(entity));
+            psUpdate.executeUpdate();
+        } catch (SQLException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteById(long id){
+        try{
+            psDeleteById.setObject(1, id);
+            psDeleteById.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAll() throws SQLException {
+        try {
+            psDeleteAll.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private Method prepareIdField() throws NoSuchMethodException {
+        var idField = Arrays.stream(cls.getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(RepositoryField.class))
+                .filter(f -> f.isAnnotationPresent(RepositoryIdField.class))
+                .findFirst().orElse(null);
+        if (idField != null) {
+            return cls.getMethod("get" + idField.getName().substring(0, 1).toUpperCase() + idField.getName().substring(1));
+        } else {
+            throw new NoSuchMethodException("ID field not found");
+        }
+    }
+}
---
+++ Homework_7/src/main/java/org/example/Account.java
@@ -0,0 +1,68 @@
+package org.example;
+
+@RepositoryTable(title = "accounts")
+public class Account {
+    @RepositoryIdField
+    @RepositoryField
+    private Long id;
+
+    @RepositoryField
+    private Long amount;
+
+    @RepositoryField(column = "account_type")
+    private String accountType;
+
+    @RepositoryField
+    private String status;
+
+    public Account(Long amount, String accountType, String status) {
+        this.amount = amount;
+        this.accountType = accountType;
+        this.status = status;
+    }
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", accountType='" + accountType + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
